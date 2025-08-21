e57e22571eede7e5234be5ab53ae0974df3d6c92
dimbagas <dimasbagas321@gmail.com>
2025-08-21 13:43:12 +0700

    update UI & sound

--- src/views/weddings/index.jsx
+++ src/views/weddings/index.jsx
@@ -6,9 +6,11 @@ import footerImage from '/image/footer-img.png';
 import cornerImage from "/image/corner-img.png";
 import backgroundTexture from "/image/bg-flower-texture.webp";
 import backgroundCard from "/image/dark-wave.jpeg";
-import cinematicWeddingAudio from "/audio/bermuara.mp3";
+// import cinematicWeddingAudio from "/audio/bermuara.mp3";
+import cinematicWeddingAudio from "/audio/cinematic-wedding.mp3";
 import { motion, AnimatePresence, useInView } from 'framer-motion';
 import { QRCodeCanvas } from 'qrcode.react';
+// import CircularGallery from '../components/CircularGallery'
 
 const FadeInUp = ({ children, delay = 0, className = '' }) => {
       const ref = useRef(null);
@@ -245,6 +247,7 @@ export default function WeddingIndex(){
     const bgCard = {
       backgroundImage: `url(${backgroundCard})`,
       backgroundPosition: 'center',
+      backgroundSize: 'cover', // Ensures the background fits the box
     };
 
     // const [norek] = useState('1360032026939');
@@ -574,20 +577,24 @@ export default function WeddingIndex(){
                     </div>
                 </div>
 
+                {/* <div style={{ height: '600px', position: 'relative' }}>
+                    <CircularGallery bend={3} textColor="#ffffff" borderRadius={0.05} scrollEase={0.02}/>
+                </div> */}
+
                 <audio ref={audioRef} autoPlay loop>
                 <source src={cinematicWeddingAudio} type="audio/mpeg" />
                 Your browser does not support the audio element.
                 </audio>
-                <button
-                    onClick={toggleMute}
-                    className="fixed bottom-4 right-4 bg-slate-500 hover:bg-slate-600 bg-opacity-50 focus:bg-opacity-50 active:bg-opacity-50 focus:outline-none active:outline-none text-white px-4 py-2 rounded-full shadow-lg"
-                    >
-                    {isMuted ? '🔇 Music Off' : '🔊 Music On'}
-                </button>
             </div>
             <div className='hidden sm:inline-block'>
                 <img src={footerImage} className='w-full bg-white opacity-60' />
             </div>
+            <button
+                onClick={toggleMute}
+                className="fixed bottom-4 right-4 bg-slate-500 hover:bg-slate-600 bg-opacity-50 focus:bg-opacity-50 active:bg-opacity-50 focus:outline-none active:outline-none text-white px-4 py-2 rounded-full shadow-lg"
+                >
+                {isMuted ? '🔇 Music Off' : '🔊 Music On'}
+            </button>
         </div>
     )
 }
